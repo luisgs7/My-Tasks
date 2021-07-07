85a2eb9d8e9922bfb01d791b748bd1833f7c0ca7
luisgs7 <luisgsilva270@gmail.com>
2021-07-07 15:48:47 -0300

    new activity task

--- app/src/main/java/com/example/mytasks/MainActivity.java
+++ app/src/main/java/com/example/mytasks/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.mytasks;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        floatingActionButton = findViewById(R.id.float_button);
+
+        floatingActionButton.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), addTask.class);
+            startActivity(intent);
+        });
     }
 }
