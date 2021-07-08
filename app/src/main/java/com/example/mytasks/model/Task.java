1f5722b4c8aeb47e4c528f9e09c44f31577b04d7
luisgs7 <luisgsilva270@gmail.com>
2021-07-08 14:28:45 -0300

    Model Task

--- app/src/main/java/com/example/mytasks/model/Task.java
+++ app/src/main/java/com/example/mytasks/model/Task.java
@@ -4,14 +4,14 @@ import java.io.Serializable;
 
 public class Task implements Serializable {
 
-    private int id;
+    private Long id;
     private String nameTask;
 
-    public int getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
